25240c3d3b61062457b2153e75bb6cc1f2b1447f
vladzvx <wladzwer@mail.ru>
2022-07-06 12:12:24 +0300

    first commit

---
+++ Lib/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Lib
+{
+    public class TestClass
+    {
+        private readonly RandomNumberGenerator generator;
+        public TestClass()
+        {
+            generator = RandomNumberGenerator.Create();
+        }
+        public string Text => "TestText";
+        public int GetInt()
+        {
+            byte[] bytes = new byte[4];
+            generator.GetBytes(bytes);
+            return BitConverter.ToInt32(bytes, 0);
+        }
+    }
+}
---
+++ Service/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Lib;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EMC.TestService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly TestClass _testClass;
+        public TestController(TestClass testClass)
+        {
+            _testClass = testClass;
+        }
+
+
+        [HttpPost()]
+        public async Task<int> Test()
+        {
+            await Task.Delay(10);
+            return _testClass.GetInt();
+        }
+    }
+}
